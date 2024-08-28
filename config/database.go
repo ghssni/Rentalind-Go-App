92a97bc8f4f9ebd7167e900213fe458a460a993a
ghssni <ghtyas25@gmail.com>
2024-08-28 21:13:15 +0700

    Fix config

--- config/database.go
+++ config/database.go
@@ -3,42 +3,36 @@ package config
 import (
 	"fmt"
 	"os"
+	"log"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"github.com/joho/godotenv"
+
+	"slc2/models"
 )
 
-// Config struct for application configuration
-type Config struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	AppPort    string
-}
-
-// LoadConfig loads the configuration from environment variables or .env file
-func LoadConfig() (*Config, error) {
-	// Load environment variables from .env file
-	err := godotenv.Load(".env")
+func InitDB()*gorm.DB{
+	err:= godotenv.Load()
 	if err != nil {
-		fmt.Println("No .env file found")
+		log.Fatal("Error Loading .env file")
 	}
 
-	// Create a new Config struct
-	config := &Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
-		AppPort:    os.Getenv("APP_PORT"),
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword,dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
 	}
 
-	// Check if all required environment variables are set
-	if config.DBHost == "" || config.DBPort == "" || config.DBUser == "" || config.DBPassword == "" || config.DBName == "" || config.AppPort == "" {
-		return nil, fmt.Errorf("missing required environment variables")
-	}
-
-	return config, nil
+	// Melakukan auto-migration untuk membuat tabel berdasarkan model yang ada
+	db.AutoMigrate(&models.User{}, &models.Vehicle{}, &models.Rental{})
+	// DB = db
+	log.Println("Database Connection Established Successfully")
+	return db
 }
